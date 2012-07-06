dfd0eac680725d0da2c9618e6c33e979494140d1
swati1591 <swati1591@gmail.com>
2012-07-06 14:47:12 +0200

    fifth commit

--- lib/cloudvmmanager/StratusAdaptor.py
+++ lib/cloudvmmanager/StratusAdaptor.py
@@ -22,7 +22,7 @@ class StratusAdaptor:
     
 
     def execscript(self, vm_ip,master):
-        s="ssh -i $PWD/$STRATUSLAB_PRIVATE_KEY -o StrictHostKeyChecking=no -l root "+vm_ip+" 'wget http://dl.dropbox.com/u/21527180/wnconfig.sh;chmod 755 wnconfig.sh;. ./wnconfig.sh "+master+"'"
+        s="ssh -i $PWD/stratuslab-client/stratuslab/.ssh/_id_rsa -o StrictHostKeyChecking=no -l root "+vm_ip+" 'wget http://dl.dropbox.com/u/21527180/wnconfig.sh;chmod 755 wnconfig.sh;. ./wnconfig.sh "+master+"'"
         File=open("wnconf.sh",'w')
         File.write(s)
         File.close()
@@ -30,12 +30,22 @@ class StratusAdaptor:
 
     def configure_vm(self,vm_ip,master):
         a="0"
+        r=StratusAdaptor()
         while a=="0":
-            time.sleep(120)
+            time.sleep(60)
             a=runCommand("stratus-describe-instance|grep "+vm_ip+"| awk '{print $4}'")
+            b=runCommand("stratus-describe-instance|grep "+vm_ip+"| awk '{print $2}'")
             a=str(a[0][0])
-        if a!="0":
+            b=str(b[0][:-1])
+            if b=="Failed":
+                break
+        if a!="0" and b!="Failed":
             time.sleep(240)
-            r=StratusAdaptor()
             StratusAdaptor.execscript(r, vm_ip, master)
             runCommand(". ./wnconf.sh")
+            return 0
+        elif b=="Failed":
+            vmid=runCommand("stratus-describe-instance|grep "+vm_ip+"| awk '{print $1}'")
+            vmid=str(vmid[0][:-1])
+            StratusAdaptor.stopvm(r,vmid)
+            return vmid
